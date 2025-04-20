075f424321cadd4109c55d16577afe84d705fb7e
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-20 14:05:57 -0400

    Actualizar Meta.js

--- plugins/Meta.js
+++ plugins/Meta.js
@@ -1,1 +1,19 @@
-//Hola
+//para matar el aburrimiento 
+let handler = async (m, { conn, text }) => {
+  if (!text) return m.reply('*_Ingrese el número del destinatario_*');
+
+  try {
+    const audio = 'https://qu.ax/TPNQE.mp3';
+    await conn.sendMessage(`${text}@s.whatsapp.net`, { audio: { url: audio }, mimetype: 'audio/mp4' });
+    m.reply('*✅ Mensaje enviado al usuario*');
+  } catch (error) {
+    m.reply('*❌ Error al enviar mensaje*');
+  }
+};
+
+handler.help = ['enviaraudio <número>'];
+handler.tags = ['General'];
+handler.command = ['enviaraud']
+handler.register = false;
+
+export default handler;
