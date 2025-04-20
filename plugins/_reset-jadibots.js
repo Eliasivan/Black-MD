25a96c167f05e1250ebfe131fee19f75562b00a0
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-20 10:53:43 -0400

    Goku-Black-Bot-MD

---
+++ plugins/_reset-jadibots.js
@@ -0,0 +1,24 @@
+/* Codigo creado por Ivan y modificado por Rayo-ofc para que no se reinicie más el host xd */
+const fs = require('fs');
+const path = require('path');
+
+let handler = async (m) => {
+  try {
+    const carpeta = path.join(__dirname, 'jadibots');
+    fs.rmdirSync(carpeta, { recursive: true });
+    m.reply('La carpeta "jadibots" ha sido eliminada con éxito');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      m.reply('La carpeta "jadibots" no existe');
+    } else {
+      m.reply(`Error al eliminar la carpeta: ${error.message}`);
+    }
+  }
+};
+
+handler.help = ['resetjadibots'];
+handler.tags = ['owner'];
+handler.command = ['resetjadibots'];
+handler.rowner = true;
+
+export default handler;
