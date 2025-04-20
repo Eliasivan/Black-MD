8c1102e69c399bbf5ba805faf954fe06fef6497a
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-20 14:42:39 -0400

    Goku-Black-Bot-MD

--- plugins/Pruebas.js
+++ plugins/Pruebas.js
@@ -1,1 +1,18 @@
-//xd
+let handler = async (m, { conn, text }) => {
+  if (!text) return m.reply('`Ingrese el mensaje que deseas enviar`');
+
+  try {
+    const channelId = '120363351515256850@newsletter';
+    await conn.sendMessage(channelId, { text: text });
+    m.reply('*✅ Mensaje enviado al canal*');
+  } catch (error) {
+    m.reply('*❌ Error al enviar mensaje*');
+  }
+};
+
+handler.help = ['enviarcanal <mensaje>'];
+handler.tags = ['General'];
+handler.command = ['envir'];
+handler.register = false;
+
+export default handler;
