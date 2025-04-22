cb0abb7ee217a94c38c67ecb8e4042344cafef5a
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-22 19:12:33 -0400

    Goku-Black-Bot-MD 💥

--- Lenguaje/en.js
+++
@@ -1,7 +0,0 @@
-export const mensajes = {
-  felicidades: "Congratulations!",
-  malaSuerte: "Bad luck",
-  admin: "You won admin in this group!",
-  eliminado: "You will be removed from the group.",
-  seleccionIdioma: "Select language: English"
-};
--- Lenguaje/es.js
+++
@@ -1,7 +0,0 @@
-export const mensajes = {
-  felicidades: "Felicidades!",
-  malaSuerte: "Mala suerte",
-  admin: "Te ganaste admin en este grupo!",
-  eliminado: "Te tendré que eliminar del grupo.",
-  seleccionIdioma: "Seleccione idioma: Español"
-};
