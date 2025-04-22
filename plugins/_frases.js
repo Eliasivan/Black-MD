6313cf72a99936e3e75d2d833ffa989300de4842
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-22 13:13:10 -0400

    Goku-Black-Bot-MD

---
+++ plugins/_frases.js
@@ -0,0 +1,23 @@
+let handler = async (m, { conn }) => {
+  let frases = ["Te rompo todo, menos el corazón",
+"No se trata de entender la vida sino de vivirla"];
+  let frase = frases[Math.floor(Math.random() * frases.length)];
+  await conn.sendMessage(m.chat, {
+    text: frase,
+    contextInfo: {
+      forwardingScore: 9999999,
+      isForwarded: false,
+      externalAdReply: {
+        title: rcanal,
+        body: "Frases inspiradoras",
+        thumbnail: imagen2
+      }
+    }
+  }, { quoted: m });
+};
+
+handler.command = ['fraset'];
+handler.tags = ['frases'];
+handler.help = ['frase'];
+
+export default handler;
