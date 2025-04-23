cde23f01f966c0598d544f3c3da22851ab1fc7e8
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-22 21:42:32 -0400

    Actualizar pinst.js

--- plugins/pinst.js
+++ plugins/pinst.js
@@ -3,16 +3,14 @@ import axios from 'axios';
 const pinterest = async (m, { conn, text }) => {
   if (!text) return conn.reply(m.chat, 'Por favor, ingresa un texto para buscar en Pinterest');
 
-  const url = `https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=%2Fsearch%2Fpins%2F%3Fq%3D${encodeURIComponent(text)}&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22query%22%3A%22${encodeURIComponent(text)}%22%2C%22scope%22%3A%22pins%22%2C%22no_fetch_context_on_resource%22%3Afalse%7D%2C%22context%22%3A%7B%7D%7D&_=1619980301559`;
+  const url = `https://api.fgmods.xyz/api/search/pinterest?text=${encodeURIComponent(text)}&apikey=fg_STZFglNn`;
 
   try {
     const response = await axios.get(url);
     const data = response.data;
 
-    if (data && data.resource_response.data.results.length > 0) {
-      const images = data.resource_response.data.results.map(image => image.images.orig.url);
-
-      for (const image of images.slice(0, 5)) {
+    if (data && data.length > 0) {
+      for (const image of data.slice(0, 5)) {
         await conn.sendFile(m.chat, image, 'pinterest.jpg', '');
       }
     } else {
@@ -20,12 +18,13 @@ const pinterest = async (m, { conn, text }) => {
     }
   } catch (error) {
     console.error(error);
-    conn.reply(m.chat, 'Error al buscar en Pinterest');
+    conn.reply(m.chat, `Error al buscar en Pinterest: ${error.message}`);
   }
 };
 
 pinterest.help = ['pinterest'];
 pinterest.tags = ['search'];
-pinterest.command = ['pind'];
+pinterest.command = ['pinterest'];
+pinterest.group = true;
 
 export default pinterest;
