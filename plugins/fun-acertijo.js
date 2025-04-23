fe6480e36da2683632ae8101319f277af81218c1
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-23 17:12:42 -0400

    Goku-Black-Bot-MD

--- plugins/fun-acertijo.js
+++ plugins/fun-acertijo.js
@@ -28,7 +28,7 @@ const handler = async (m, {conn, usedPrefix}) => {
 };
 handler.help = ['acertijo'];
 handler.tags = ['fun'];
-handler.group = true;
+handler.group = disable;
 handler.register = true
 handler.command = ['acertijo','acert','adivinanza','tekateki']
 export default handler;
