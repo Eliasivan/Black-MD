95ce62f221a25de00bdaa54d12f0dc16ad5e5495
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-23 17:33:59 -0400

    Goku-Black-Bot-MD

--- plugins/fun-acertijo.js
+++ plugins/fun-acertijo.js
@@ -1,38 +1,33 @@
 import fs from 'fs';
-
-const timeout = 60000;
+const timeout = 30000;
 const poin = 10;
-
-const handler = async (m, { conn, usedPrefix }) => {
+const handler = async (m, {conn, usedPrefix}) => {
   conn.tekateki = conn.tekateki ? conn.tekateki : {};
   const id = m.chat;
-
   if (id in conn.tekateki) {
-    conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0]);
-    return;
+    conn.reply(m.chat, '*Todavía hay acertijos sin responder en este chat*', conn.tekateki[id][0]);
+    throw false;
   }
-
-  const tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`));
+  const tekateki = JSON.parse(fs.readFileSync(`./media/database/acertijo.json`));
   const json = tekateki[Math.floor(Math.random() * tekateki.length)];
   const _clue = json.response;
   const clue = _clue.replace(/[A-Za-z]/g, '_');
-  const caption = `🚩 *ACERTIJOS* ✨️\n*${json.question}*\n⏱️ *Tiempo:* ${(timeout / 1000).toFixed(2)} Segundos\n🎁 *Premio:* *+${poin}* Centavos 🪙`.trim();
+  const caption = `
+ⷮ💛 *ACERTIJOS*
+✨️ *${json.question}*
 
+⏱️ *Tiempo:* ${(timeout / 1000).toFixed(2)} Segundos
+☁ *Premio:* *+${poin}* Centavos 🪙`.trim();
   conn.tekateki[id] = [
-    await conn.sendMessage(m.chat, { text: caption }),
-    json,
+    await conn.reply(m.chat, caption, m), json,
     poin,
     setTimeout(async () => {
-      if (conn.tekateki[id]) {
-        await conn.sendMessage(m.chat, { text: `🤍 Se acabó el tiempo!\n*Respuesta:* ${json.response}` });
-        delete conn.tekateki[id];
-      }
-    }, timeout)
-  ];
+      if (conn.tekateki[id]) await conn.reply(m.chat, `🌸 Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0]);
+      delete conn.tekateki[id];
+    }, timeout)];
 };
-
 handler.help = ['acertijo'];
 handler.tags = ['fun'];
-handler.command = ['acertijo', 'acert', 'adivinanza', 'tekateki'];
-
+handler.command = /^(acertijo|acert|adivinanza|tekateki)$/i;
+handler.estrellas = 3;
 export default handler;
