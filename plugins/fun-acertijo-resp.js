69ba08b3aabaf9db4ca6678a8bb35fc4615290d2
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-23 17:35:13 -0400

    Goku-Black-Bot-MD

--- plugins/fun-acertijo-resp.js
+++ plugins/fun-acertijo-resp.js
@@ -1,30 +1,22 @@
+import similarity from 'similarity';
+const threshold = 0.72;
+const handler = (m) => m;
 handler.before = async function(m) {
   const id = m.chat;
-
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^🚩/i.test(m.quoted.text)) return !0;
-
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-
   if (!(id in this.tekateki)) return m.reply('✨️ Ese acertijo ya ha terminado!');
-
-  if (m.quoted.id == this.tekateki[id][0].key.id) {
+  if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
-    const userResponse = m.text.trim().toLowerCase();
-    const correctResponse = json.response.trim().toLowerCase();
-
-    console.log(`Respuesta del usuario: ${userResponse}`);
-    console.log(`Respuesta correcta: ${correctResponse}`);
-
-    if (userResponse == correctResponse) {
+    if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
       global.db.data.users[m.sender].estrellas += this.tekateki[id][2];
-      m.reply(`🎉 *Respuesta correcta!*\n+${this.tekateki[id][2]} Centavos`);
+      m.reply(`[🌠] *Respuesta correcta!*\n+${this.tekateki[id][2]} Estrellas`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
-    } else if (similarity(userResponse, correctResponse) >= threshold) {
-      m.reply(`🤔 Casi lo logras!`);
-    } else {
-      m.reply('❌ Respuesta incorrecta!');
-    }
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`);
+    else m.reply('Respuesta incorrecta!');
   }
   return !0;
 };
+handler.exp = 0;
+export default handler;
