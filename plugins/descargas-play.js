35d8d2b896a8f566f6e1f1c2169ea2d4d6853c5c
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-23 22:41:24 -0400

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,29 +1,54 @@
 import axios from 'axios';
-import { URL } from 'url';
+import yts from 'yt-search';
 
 const handler = async (m, { conn, args }) => {
-  if (!args[0]) return m.reply('Por favor, proporciona una URL de YouTube válida');
+  if (!args[0]) return m.reply('Ingresa el link del YouTube o nombre para descargarlo');
 
-  const url = args[0];
-  const apikey = 'sylph'; // Reemplaza con tu propia API key si es necesario
+  const text = args.join(' ');
+  let url;
+
+  if (text.includes('youtube.com') || text.includes('youtu.be')) {
+    url = text;
+  } else {
+    const search = await yts(text);
+    if (!search.all || search.all.length === 0) {
+      return m.reply('No se encontraron resultados para tu búsqueda.');
+    }
+    url = search.all[0].url;
+  }
+
+  const videoInfo = await yts(url);
+  const { title, thumbnail, timestamp, views, ago } = videoInfo.videos[0];
+
+  const infoMessage = `*Información del audio:*\n\n*Título:* ${title}\n*Duración:* ${timestamp}\n*Vistas:* ${views}\n*Publicado:* ${ago}`;
+
+  m.reply(infoMessage);
+
+  const apikey = 'sylph'; // API key válida
   const apiUrl = `https://api.sylphy.xyz/download/ytmp3?url=${encodeURIComponent(url)}&apikey=${apikey}`;
 
   try {
     m.react('⏳'); // Reacción de tiempo
     const response = await axios.get(apiUrl);
-    const data = response.data;
 
-    if (data.status) {
-      const audioUrl = data.result.download_url;
-      const audioTitle = data.result.title;
+    console.log(response.data); // Verificar la respuesta de la API
 
-      m.reply(`Descargando audio... ${audioTitle}`);
-      await conn.sendFile(m.chat, audioUrl, audioTitle + '.mp3', '', m);
-      m.react('✅'); // Reacción de verificación
-      m.reply('Audio descargado y enviado con éxito!');
+    if (response.status === 200 && response.data.status === true) {
+      const audioUrl = response.data.result.download_url;
+      const audioTitle = response.data.result.title;
+
+      if (audioUrl && audioTitle) {
+        await conn.sendFile(m.chat, audioUrl, `${audioTitle}.mp3`, '', m);
+        m.react('✅'); // Reacción de verificación
+        m.reply('Audio descargado y enviado con éxito!');
+      } else {
+        m.react('❌'); // Reacción de error
+        m.reply('Error al descargar el audio');
+      }
     } else {
       m.react('❌'); // Reacción de error
-      m.reply('Error al descargar el audio');
+      m.reply('Error al procesar la solicitud');
+      console.log(response.data); // Verificar la respuesta de la API
     }
   } catch (error) {
     console.error(error);
@@ -34,6 +59,6 @@ const handler = async (m, { conn, args }) => {
 
 handler.help = ['ytmp3 <url>'];
 handler.tags = ['downloader'];
-handler.command = ['ytmp3', 'yta3'];
+handler.command = ['ytmp3', 'yta'];
 
 export default handler;
