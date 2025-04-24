4c8c8f6b996bec43cdf71e0aaf68e1b4acf3abae
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-24 17:00:46 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,47 +1,31 @@
 import { Sticker } from 'wa-sticker-formatter'
 
 let handler = async (m, { conn }) => {
-  if (!m.quoted || !/image|video|sticker/.test(m.quoted.mimetype)) return m.reply('🚩 Menciona una imagen, video o GIF para convertirla en sticker.')
+  if (!m.quoted || !/image|video/.test(m.quoted.mimetype)) return m.reply('🚩 Menciona una imagen o video para convertirla en sticker.')
   
-  let type = m.text.split(' ')[0].toLowerCase()
-  let options = {
+  let comando = m.text.split(' ')[0].toLowerCase()
+  let opciones = m.text.split(' ')
+  
+  let buffer = await m.quoted.download()
+  let sticker = new Sticker(buffer, {
     pack: 'Tylarz',
     author: 'Sticker creado con Tylarz',
+  })
+  
+  if (opciones.includes('-i')) {
+    sticker.setImage(buffer)
+  } else if (opciones.includes('-x')) {
+    sticker.crop = false
+  } else if (opciones.includes('-c')) {
+    sticker.circle = true
   }
   
-  if (type === '.s') {
-    let buffer = await m.quoted.download()
-    let sticker = new Sticker(buffer, options)
-    await sticker.build()
-    await conn.sendMessage(m.chat, { sticker: await sticker.getBuffer() })
-  } else if (type === '.s' && m.text.includes('-i')) {
-    // Sticker ampliado
-    let buffer = await m.quoted.download()
-    let sticker = new Sticker(buffer, { ...options, isImage: true })
-    await sticker.build()
-    await conn.sendMessage(m.chat, { sticker: await sticker.getBuffer() })
-  } else if (type === '.s' && m.text.includes('-x')) {
-    // Sticker acoplado
-    let buffer = await m.quoted.download()
-    let sticker = new Sticker(buffer, { ...options, crop: false })
-    await sticker.build()
-    await conn.sendMessage(m.chat, { sticker: await sticker.getBuffer() })
-  } else if (type === '.s' && m.text.includes('-c')) {
-    // Sticker circular
-    let buffer = await m.quoted.download()
-    let sticker = new Sticker(buffer, { ...options, circle: true })
-    await sticker.build()
-    await conn.sendMessage(m.chat, { sticker: await sticker.getBuffer() })
-  } else {
-    m.reply(`🚩 Opción inválida. Las opciones disponibles son:
-.s (sticker normal)
-.s -i (sticker ampliado)
-.s -x (sticker acoplado)
-.s -c (sticker circular)`)
-  }
+  await sticker.build()
+  let stickerBuffer = await sticker.getBuffer()
+  await conn.sendMessage(m.chat, { sticker: stickerBuffer })
 }
 
-handler.help = ['.s', '.s -i', '.s -x', '.s -c']
+handler.help = ['.s']
 handler.tags = ['sticker']
 handler.command = /^(\.s)$/i
 
