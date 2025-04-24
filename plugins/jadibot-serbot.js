c90f8168c601c0ad1fe1c474114ce9ab2dfdf047
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-04-24 12:18:16 -0400

    Goku-Black-Bot-MD

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -120,14 +120,25 @@ sock.isInit = false
 let isInit = true
 
 async function connectionUpdate(update) {
-const { connection, lastDisconnect, isNewLogin, qr } = update
-if (isNewLogin) sock.isInit = false
-if (qr && !mcode) {
-txtQR = await conn.sendMessage(m.chat, { image: await qrcode.toBuffer(qr, { scale: 8 }), caption: rtx.trim()}, { quoted: m})
-return
-} 
-if (qr && mcode) {
-txtCode = await conn.sendMessage(m.chat, {text : rtx2}, { quoted: m })
+  const { connection, lastDisconnect, isNewLogin, qr } = update
+  if (isNewLogin) sock.isInit = false
+  if (qr && !mcode) {
+    // ...
+  }
+  if (qr && mcode) {
+    let secret = await sock.requestPairingCode((m.sender.split`@`[0]))
+    secret = secret.match(/.{1,4}/g)?.join("-")
+    const buttonMessage = {
+      text: rtx2.trim() + '\n' + drmer.toString("utf-8") + '\n' + wm + `\n*Código:* ${secret}`,
+      buttons: [
+        {
+          buttonId: `copy_code`,
+          buttonText: { displayText: 'Copiar código' },
+          type: 1
+        }
+      ]
+    }
+    txtCode = await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 await sleep(3000)
 let secret = await sock.requestPairingCode((m.sender.split`@`[0]))
 secret = secret.match(/.{1,4}/g)?.join("-")
