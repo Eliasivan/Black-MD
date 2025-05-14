47ae8fb16161b5721dee11ada732905c6b9b5414
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-05-14 18:06:52 -0300

    Actualizar herramientas-translate.js

--- plugins/herramientas-translate.js
+++ plugins/herramientas-translate.js
@@ -1,43 +1,42 @@
 import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
-const handler = async (m, {args, usedPrefix, command}) => {
+
+const handler = async (m, { args, usedPrefix, command }) => {
   const msg = `👑 *Uso correcto del comando ${usedPrefix + command} (idioma) (texto)*\n*Ejemplo:*\n*${usedPrefix + command} es Hello*\n\n*Conoce los idiomas admitidos en:*\n*- https://cloud.google.com/translate/docs/languages*`;
   if (!args || !args[0]) return m.reply(msg);
+
   let lang = args[0];
   let text = args.slice(1).join(' ');
   const defaultLang = 'es';
+
+  // Establecer idioma por defecto si no se proporciona un código válido
   if ((args[0] || '').length !== 2) {
     lang = defaultLang;
     text = args.join(' ');
   }
+
+  // Si no hay texto explícito, intentar usar texto citado
   if (!text && m.quoted && m.quoted.text) text = m.quoted.text;
+
   try {
-    conn.reply(m.chat, wait, m, {
-    contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-    title: packname,
-    body: wm,
-    previewType: 0, thumbnail: icons,
-    sourceUrl: redes }}})
-    const result = await translate(`${text}`, {to: lang, autoCorrect: true});
+    // Intentar traducir con la primera API
+    const result = await translate(`${text}`, { to: lang, autoCorrect: true });
     await m.reply('*Traducción:* ' + result.text);
   } catch {
     try {
-    conn.reply(m.chat, wait, m, {
-    contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-    title: packname,
-    body: wm,
-    previewType: 0, thumbnail: icons,
-    sourceUrl: redes }}})
+      // Intentar traducir con API alternativa si falla la primera
       const lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=${lolkeysapi}&text=${text}`);
       const loll = await lol.json();
       const result2 = loll.result.translated;
       await m.reply('*Traducción:* ' + result2);
     } catch {
-      await m.reply('✨️ *Ocurrió Un Error*');
+      // Manejo de errores genérico
+      await m.reply('✨️ *Ocurrió un error al traducir el texto.*');
     }
   }
 };
-handler.command = ['translate','traducir','trad'];
+
+handler.command = ['translate', 'traducir', 'trad'];
 handler.group = true;
-handler.register = true
+handler.register = true;
 export default handler;
