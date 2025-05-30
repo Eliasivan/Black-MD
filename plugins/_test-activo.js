aeab77cb8c5bcf6f0160b85360e066eba0764ade
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-05-30 00:19:15 -0300

    Actualizar _test-activo.js

--- plugins/_test-activo.js
+++ plugins/_test-activo.js
@@ -3,20 +3,18 @@ import fetch from 'node-fetch';
 const handler = async (m, { conn, text }) => {
     const mensaje = "Estoy activo ✅";
 
-    if (text.toLowerCase() === "activo" || text.toLowerCase() === "p") {
+    if (text && (text.toLowerCase() === "activo" || text.toLowerCase() === "p")) {
         await conn.sendMessage(m.chat, { 
             text: mensaje,
             contextInfo: {
                 thumbnailUrl: 'https://files.catbox.moe/0ous50.jpg',
                 newsletterJid: '12345@s.whatsapp.net',
-                newsletterName: 'Goku-Black-Bot-MD',
+                newsletterName: 'GokuBlack',
             }
         });
         await m.react('👍🏻');
     }
 };
 
-handler.tags = ['info'];
-handler.command = /^activo|p$/i;
-
 export default handler;
+```
