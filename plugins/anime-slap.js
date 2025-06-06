97c048a8fedf201213fd6190cda0f3a5efd84708
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-06-06 18:41:38 -0300

    Actualizar anime-slap.js

--- plugins/anime-slap.js
+++ plugins/anime-slap.js
@@ -7,13 +7,21 @@ let handler = async (m, { conn, usedPrefix }) => {
     } else {
         who = m.chat;
     }
-    if (!who) throw `🚩 Por favor`, { react: { text: '👊🏻', key: m.key } });
+    if (!who) throw `🚩 Por favor, etiqueta o menciona a alguien.\n💡 Usa: ${usedPrefix}bofetada @usuario`;
 
-    let str = `${name2} ha golpeado a ${name}`.trim();
+    let name = conn.getName(who);
+    let name2 = conn.getName(m.sender);
+
+    await conn.sendMessage(m.chat, { react: { text: '👊🏻', key:.trim();
 
     try {
         let response = await fetch(`https://api.waifu.pics/sfw/slap`);
-        if (!response.ok) throw `❌ Error`, who] }, { quoted: m });
+        if (!response.ok) throw `❌ Error al obtener datos de la API.`;
+
+        let jsonData = await response.json();
+        let { url } = jsonData;
+
+        await conn.sendMessage(m.chat, { image: { url }, caption: str, mentions: [m.sender, who] }, { quoted: m });
     } catch (error) {
         throw `❌ Ocurrió un error: ${error}`;
     }
