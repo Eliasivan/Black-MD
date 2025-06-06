fd54e399ac8b27b2df79bb67961a8f4d951d0167
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-06-06 17:45:01 -0300

    Goku-Black-Bot-MD

--- plugins/Tus grupos.js
+++ plugins/Tus grupos.js
@@ -1,15 +1,11 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    if (!global.db.data.chats[m.chat].nsfw) throw `🚫 ${mssg.gpNsfw(usedPrefix)}`
-    let user = global.db.data.users[m.sender].age
-    if (user < 17) throw `❎ ${mssg.nsfwAge}`
-
     m.react(rwait)
 
     if (command === 'pussy') {
         let xp = await conn.getFile(global.API('fgmods', '/api/nsfw/pussy', {}, 'apikey'))
-        conn.sendFile(m.chat, xp.data, 'img ${mssg.random} *${command}*`, m)
+        conn.sendFile(m.chat, xp.data, 'img.jpg', `${mssg.random} *${command}*`, m)
         m.react(xmoji)
     } else {
         throw `❎ ${mssg.invalidCommand}`
@@ -18,9 +14,6 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
 handler.help = ['pussy']
 handler.tags = ['nsfw']
-handler.command = /^pussy$/i // Solo acepta el comando "pussy"
-handler.diamond = true
-handler.register = true
-handler.group = true
+handler.command = ['pussy']
 
 export default handler
