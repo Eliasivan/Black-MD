e809c0782a7ca06b2a6a523aa03bc463d4c7761a
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-06-06 17:52:32 -0300

    Actualizar Tus grupos.js

--- plugins/Tus grupos.js
+++ plugins/Tus grupos.js
@@ -5,12 +5,16 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
     if (command === 'pussy') {
         let xp = await conn.getFile('https://api.fgmods.xyz/api/nsfw-nime/pussy?apikey=fg_ZIKajBcu')
-        conn.sendFile(m.chat, xp.data, 'img.jpg', `${mssg.random} *${command}*`, m)
+        conn.sendFile(m.chat, xp.data, 'img.jpg', `✅ Aquí tienes unacommand}*`, m)
         m.react(xmoji)
-    }}
+    } else {
+        throw `Comando no válido. Usa: ${usedPrefix}pussy`
+    }
+}
 
 handler.help = ['pussy']
 handler.tags = ['nsfw']
 handler.command = ['pussy']
 
 export default handler
+
