e98f0cea786651e10a7296c3226061044bd7f8e2
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-06-06 17:54:56 -0300

    Goku-Black-Bot-MD

--- plugins/Tus grupos.js
+++ plugins/Tus grupos.js
@@ -5,8 +5,8 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
     if (command === 'pussy') {
         let xp = await conn.getFile('https://api.fgmods.xyz/api/nsfw-nime/pussy?apikey=fg_ZIKajBcu')
-        conn.sendFile(m.chat, xp.data, 'img.jpg', `✅ Aquí tienes una Pussy*`, m)
-        m.react(xmoji)
+        conn.sendFile(m.chat, xp.data, 'img.jpg', `*✅ Aquí tienes una Pussy*`, m)
+        m.react('🤗');
     } else {
         throw `Comando no válido. Usa: ${usedPrefix}pussy`
     }
