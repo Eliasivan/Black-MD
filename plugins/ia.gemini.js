a0cf788731cea8a2d25c05313cfc492e8f3dc042
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-06-08 11:55:37 -0300

    Goku-Black-Bot-MD

--- plugins/ia.gemini.js
+++ plugins/ia.gemini.js
@@ -1,19 +1,36 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
-var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `✨ *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} hola, conoces a Goku-Black-Bot-MD?`, m)
-try {
-await m.react('🕒')
-conn.sendPresenceUpdate('composing', m.chat)
-var apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${text}`)
-var res = await apii.json()
-await conn.reply(m.chat, res.result, m)
-await m.react('✅️')
-} catch (error) {
-return conn.reply(m.chat, 'Enseñame que responder.', m)
-}}
+var handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) {
+        return conn.reply(
+            m.chat,
+            `✨ *Ingresé una petición*\n\nEjemplo: ${usedPrefix + command} hola, conoces a Goku-Black-Bot-MD?`,
+            m
+        );
+    }
+    try {
+        await m.react('🕒');
+        conn.sendPresenceUpdate('composing', m.chat);
 
-handler.command = ['gemini']
-handler.help = ['gemini']
-handler.tags = ['ai']
-export default handler
+        var apii.app/starlight/gemini?text=${text}`);
+        var res = await apii.json();
+
+        const responseMessage = `✨ *Creador*: Ivan\n\n${res.result}\n\n≡ *Grandes de los bots en aquella época:* 
+┌──────────────
+▢ Ender Lobo-Bot-MD
+▢ Jostin Goku-Bot-MD
+▢ Axx Baileys-Bot-MD
+▢ Enzo Diabla-Bot-MD
+└──────────────`;
+
+        await conn);
+        await m.react('✅️');
+    } catch (error) {
+        return conn.reply(m.chat, 'Enseñame que responder.', m);
+    }
+};
+
+handler.command = ['gemini'];
+handler.help = ['gemini'];
+handler.tags = ['ai'];
+export default handler;
