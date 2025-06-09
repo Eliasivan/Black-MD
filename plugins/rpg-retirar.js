2c293917dded2ed1762b98d868e47b3ec458a5e4
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-06-08 23:22:58 -0300

    Goku-Black-Bot-MD

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -1,26 +1,29 @@
 import db from '../lib/database.js'
 
 let handler = async (m, { args }) => {
-let user = global.db.data.users[m.sender]
-if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *⭐ Estrellas* que deseas Retirar.')
-if (args[0] == 'all') {
-let count = parseInt(user.bank)
-user.bank -= count * 1
-user.estrellas += count * 1
-await m.reply(`🚩 Retiraste *${count} ⭐ Estrellas* del Banco.`)
-return !0
+    let user = global.db.data.users[m.sender]
+    if (!args[0]) return m.reply(`✨ Ingresa la cantidad de *estrellas* que deseas retirar.`)
+    if (args[0] == 'all') {
+        let count = parseInt(user.bank || 0)
+        user.bank -= count * 1
+        user.estrella = (user.estrella || 0) + count * 1
+        await m.reply(`🌟 Retiraste *${count} estrellas* del banco. Ahora podrás usarlas, pero también estarán expuestas a robos.`)
+        return true
+    }
+    if (!Number(args[0])) return m.reply(`❌ Debes retirar una cantidad válida.\n> Ejemplo 1 » *#retirar 25000*\n> Ejemplo 2 » *#retirar all*`)
+    let count = parseInt(args[0])
+    if (!user.bank || user.bank < count) {
+        return m.reply(`❌ No tienes suficientes estrellas en el banco. Actualmente tienes *${user.bank || 0} estrellas*.`)
+    }
+    user.bank -= count * 1
+    user.estrella = (user.estrella || 0) + count * 1
+    await m.reply(`🌟 Retiraste *${count} estrellas* del banco. Ahora podrás usarlas, pero también estarán expuestas a robos.`)
 }
-if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
-let count = parseInt(args[0])
-if (!user.bank) return m.reply('No tienes *⭐ Estrellas* en el Banco.')
-if (user.bank < count) return m.reply(`Solo tienes *${user.bank} ⭐ Estrellas* en el Banco.`)
-user.bank -= count * 1
-user.estrellas += count * 1
-await m.reply(`🚩 Retiraste *${count} ⭐ Estrellas* del Banco.`)}
 
 handler.help = ['retirar']
 handler.tags = ['rpg']
-handler.command = ['withdraw', 'retirar', 'wd']
-handler.group = true;
+handler.command = ['withdraw', 'retirar', 'with']
+handler.group = true
 handler.register = true
+
 export default handler
