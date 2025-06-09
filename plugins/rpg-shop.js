307ea83588fd6940df8d26cca747bcd6204fc6f0
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-06-08 23:42:50 -0300

    Goku-Black-Bot-MD

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -1,27 +1,26 @@
-const estrellas = 350;
+const xppercoin = 350;
 const handler = async (m, {conn, command, args}) => {
   let count = command.replace(/^buy/i, '');
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / estrellas) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xppercoin) : parseInt(count) : args[0] ? parseInt(args[0]) : 1;
   count = Math.max(1, count);
-  if (global.db.data.users[m.sender].exp >= estrellas * count) {
-    global.db.data.users[m.sender].exp -= estrellas * count;
-    global.db.data.users[m.sender].estrellas += count;
+  if (global.db.data.users[m.sender].exp >= xppercoin * count) {
+    global.db.data.users[m.sender].exp -= xppercoin * count;
+    global.db.data.users[m.sender].coin += count;
     conn.reply(m.chat, `
 ╔═══════⩽✰⩾═══════╗
 ║    𝐍𝐨𝐭𝐚 𝐃𝐞 𝐏𝐚𝐠𝐨 
 ╠═══════⩽✰⩾═══════╝
 ║╭──────────────┄
-║│ *Compra Nominal* : + ${count}🌟
-║│ *Gastado* : -${estrellas * count} XP
+║│ *Compra Nominal* : + ${count} 💸
+║│ *Gastado* : -${xppercoin * count} XP
 ║╰──────────────┄
-╚═══════⩽✰⩾═══════╝`, m, rcanal);
-  } else conn.reply(m.chat, `😔 Lo siento, no tienes suficiente *XP* para comprar *${count}* Estrellas 🌟`, m, rcanal);
+╚═══════⩽✰⩾═══════╝`, m);
+  } else conn.reply(m.chat, `${emoji2} Lo siento, no tienes suficiente *XP* para comprar *${count}* ${moneda} 💸`, m);
 };
 handler.help = ['Buy', 'Buyall'];
-handler.tags = ['xp'];
-handler.register = true
+handler.tags = ['economy'];
 handler.command = ['buy', 'buyall'];
-
-handler.disabled = false;
+handler.group = true;
+handler.register = true;
 
 export default handler;
