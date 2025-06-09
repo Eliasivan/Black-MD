7fc55d8dc51d350c85749ab00f47e2d4eaed0e58
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-06-09 12:13:00 -0300

    Goku-Black-Bot-MD

--- plugins/fun-ruleta.js
+++ plugins/fun-ruleta.js
@@ -1,64 +1,64 @@
 let cooldowns = {}
 
 let handler = async (m, { conn, text, command, usedPrefix }) => {
-  let users = global.db.data.users[m.sender]
+    let users = global.db.data.users[m.sender]
 
-  let tiempoEspera = 10
+    let tiempoEspera = 10
 
-  if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
-    let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-    conn.reply(m.chat, `🚩 Ya has iniciado una apuesta recientemente, espera *⏱ ${tiempoRestante}* para apostar nuevamente`, m, rcanal)
-    return
-  }
-
-  cooldowns[m.sender] = Date.now()
-
-  if (!text) return conn.reply(m.chat, `🚩 Debes ingresar una cantidad de *🍪 Cookies* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m, rcanal)
-
-  let args = text.trim().split(" ")
-  if (args.length !== 2) return conn.reply(m.chat, `🚩 Formato incorrecto. Debes ingresar una cantidad de *🍪 Cookies* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m, rcanal)
-
-  let cookies = parseInt(args[0])
-  let color = args[1].toLowerCase()
-
-  if (isNaN(cookies) || cookies <= 0) return conn.reply(m.chat, `🚩 Por favor, ingresa una cantidad válida para la apuesta.`, m, rcanal)
-
-  if (cookies > 50) return conn.reply(m.chat, "🚩 La cantidad máxima de apuesta es de 50 *🍪 Cookies*.", m, rcanal)
-
-  if (!(color === 'black' || color === 'red')) return conn.reply(m.chat, "🚩 Debes apostar a un color válido: *black* o *red*.", m, rcanal)
-
-  if (cookies > users.cookies) return conn.reply(m.chat, "🚩 No tienes suficientes *🍪 Cookies* para realizar esa apuesta.", m, rcanal)
-
-  await conn.reply(m.chat, `🚩 Apostaste ${estrellas} *🍪 Cookies* al color ${color}. Espera *⏱ 10 segundos* para conocer el resultado.`, m, rcanal)
-
-  setTimeout(() => {
-    let result = Math.random()
-    let win = false
-
-    if (result < 0.5) {
-      win = color === 'black'
-    } else {
-      win = color === 'red'
+    if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
+        let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
+        conn.reply(m.chat, `🚩 Ya has iniciado una apuesta recientemente, espera *⏱ ${tiempoRestante}* para apostar nuevamente.`, m)
+        return
     }
 
-    if (win) {
-      users.cookies += cookies
-      conn.reply(m.chat, `🚩 ¡Ganaste! Obtuviste ${estrellas} *🍪 Cookies*. Total: ${users.estrellas} *🍪 Cookies*.`, m, rcanal)
-    } else {
-      users.cookies -= cookies
-      conn.reply(m.chat, `🚩 Perdiste. Se restaron ${estrellas} *🍪 Cookies*. Total: ${users.estrellas} *🍪 Cookies*.`, m, rcanal)
-    }
+    cooldowns[m.sender] = Date.now()
 
+    if (!text) return conn.reply(m.chat, `🚩 Debes ingresar una cantidad de *🌟 estrellas* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m)
 
-  }, 10000)
+    let args = text.trim().split(" ")
+    if (args.length !== 2) return conn.reply(m.chat, `🚩 Formato incorrecto. Debes ingresar una cantidad de *🌟 estrellas* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m)
+
+    let estrellas = parseInt(args[0])
+    let color = args[1].toLowerCase()
+
+    if (isNaN(estrellas) || estrellas <= 0) return conn.reply(m.chat, `🚩 Por favor, ingresa una cantidad válida para la apuesta.`, m)
+
+    if (estrellas > 50) return conn.reply(m.chat, "🚩 La cantidad máxima de apuesta es de 50 *🌟 estrellas*.", m)
+
+    if (!(color === 'black' || color === 'red')) return conn.reply(m.chat, "🚩 Debes apostar a un color válido: *black* o *red*.", m)
+
+    if (estrellas > users.estrellas) return conn.reply(m.chat, "🚩 No tienes suficientes *🌟 estrellas* para realizar esa apuesta.", m)
+
+    await conn.reply(m.chat, `🚩 Apostaste ${estrellas} *🌟 estrellas* al color ${color}. Espera *⏱ 10 segundos* para conocer el resultado.`, m)
+
+    setTimeout(() => {
+        let result = Math.random()
+        let win = false
+
+        if (result < 0.5) {
+            win = color === 'black'
+        } else {
+            win = color === 'red'
+        }
+
+        if (win) {
+            users.estrellas += estrellas
+            conn.reply(m.chat, `🚩 ¡Ganaste! Obtuviste ${estrellas} *🌟 estrellas*. Total: ${users.estrellas} *🌟 estrellas*.`, m)
+        } else {
+            users.estrellas -= estrellas
+            conn.reply(m.chat, `🚩 Perdiste. Se restaron ${estrellas} *🌟 estrellas*. Total: ${users.estrellas} *🌟 estrellas*.`, m)
+        }
+
+    }, 10000)
 }
+
 handler.tags = ['fun']
-handler.help =['ruleta *<cantidad> <color>*']
+handler.help = ['ruleta *<cantidad> <color>*']
 handler.command = ['ruleta', 'roulette', 'rt']
 handler.register = true
 export default handler
 
 function segundosAHMS(segundos) {
-  let segundosRestantes = segundos % 60
-  return `${segundosRestantes} segundos`
+    let segundosRestantes = segundos % 60
+    return `${segundosRestantes} segundos`
 }
