794184c254e5bfa35649e17e60950c2713a63b7d
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-06-09 22:35:20 -0300

    Actualizar rpg-depositar.js

--- plugins/rpg-depositar.js
+++ plugins/rpg-depositar.js
@@ -1,28 +1,28 @@
+import db from '../lib/database.js'
+
 let handler = async (m, { args }) => {
-    let user = global.db.data.users[m.sender];
-    if (!args[0]) return m.reply(`✨ Ingresa la cantidad de *estrellas* que deseas depositar.`);
-    if ((args[0]) < 1) return m.reply(`✨ Ingresa una cantidad válida de *estrellas*.`);
-    if (args[0] == 'all') {
-        let count = parseInt(user.estrella || 0);
-        user.estrella -= count * 1;
-        user.bank += count * 1;
-        await m.reply(`🌟 Depositaste *${count} estrellas* en el banco. Ahora están protegidas y no podrán ser robadas.`);
-        return true;
-    }
-    if (!Number(args[0])) return m.reply(`❌ Debes depositar una cantidad válida.\n> Ejemplo 1 » *#d 25000*\n> Ejemplo 2 » *#d all*`);
-    let count = parseInt(args[0]);
-    if (!user.estrella || user.estrella < count) {
-        return m.reply(`❌ No tienes suficientes estrellas en la cartera. Actualmente tienes *${user.estrella || 0} estrellas*.`);
-    }
-    user.estrella -= count * 1;
-    user.bank += count * 1;
-    await m.reply(`🌟 Depositaste *${count} estrellas* en el banco. Ahora están protegidas y no podrán ser robadas.`);
-};
+let user = global.db.data.users[m.sender]
+if (!args[0]) return m.reply(`${emoji} Ingresa la cantidad de *${moneda}* que deseas Depositar.`)
+if ((args[0]) < 1) return m.reply(`${emoji} Ingresa una cantidad válida de *${moneda}*.`)
+if (args[0] == 'all') {
+let count = parseInt(user.coin)
+user.coin -= count * 1
+user.bank += count * 1
+await m.reply(`${emoji} Depositaste *${count} ${moneda}* en el banco, ya no podran robartelo.`)
+return !0
+}
+if (!Number(args[0])) return m.reply(`${emoji2} Debes depositar una cantidad válida.\n> Ejemplo 1 » *#d 25000*\n> Ejemplo 2 » *#d all*`)
+let count = parseInt(args[0])
+if (!user.coin) return m.reply(`${emoji2} No tienes suficientes *${moneda}* la Cartera.`)
+if (user.coin < count) return m.reply(`${emoji2} Solo tienes *${user.coin} ${moneda}* en la Cartera.`)
+user.coin -= count * 1
+user.bank += count * 1
+await m.reply(`${emoji} Depositaste *${count} ${moneda}* en el banco, ya no podran robartelo.`)}
 
-handler.help = ['depositar'];
-handler.tags = ['rpg'];
-handler.command = ['deposit', 'depositar', 'd', 'aguardar'];
-handler.group = true;
-handler.register = true;
+handler.help = ['depositar']
+handler.tags = ['rpg']
+handler.command = ['deposit', 'depositar', 'd', 'aguardar']
+handler.group = true
+handler.register = true
 
-export default handler;
+export default handler 
