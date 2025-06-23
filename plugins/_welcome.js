cfd6318e368722ce9be0bebb977b785357d83cb6
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-06-22 23:42:55 -0300

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,29 +1,33 @@
+
 import { WAMessageStubType } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, { conn, participants, groupMetadata }) {
-  if (!m.messageStubType || !m.isGroup) return !0;
-  const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"}  
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
-  let img = await (await fetch(`${pp}`)).buffer()
+  if (!m.messageStubType || !m.isGroup) return true
+
+  let who = m.messageStubParameters[0]
+  let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let txt = 'ゲ◜៹ New user ៹◞ゲ'
-  let txt1 = 'ゲ◜៹ Bye user ៹◞ゲ'
-  let groupSize = participants.length
-  if (m.messageStubType == 27) {
-    groupSize++;
-  } else if (m.messageStubType == 28 || m.messageStubType == 32) {
-    groupSize--;
+  let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg';
+
+  if (chat.welcome) {
+    let img;
+    try {
+      let pp = await conn.profilePictureUrl(who, 'image');
+      img = await (await fetch(pp)).buffer();
+    } catch {
+      img = await (await fetch(defaultImage)).buffer();
+    }
+
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+      let bienvenida = `🌱 *Bienvenido* a ${groupMetadata.subject}\n ${taguser}\n 
+ahora somos ${groupSize} Disfruta tu estadía en el grupo!`
+      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
+    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
+      let bye = `💮 *Adiós* De ${groupMetadata.subject}\n ${taguser}\n Quedamos {groupSize} Te esperamos pronto!`
+      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
+    }
   }
 
-  if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `*Bienvenido* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✦ Ahora somos ${groupSize} Miembros.
-Disfruta tu estadía en el grupo!`    
-    await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
-  }
-  
-  if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = `*Adiós* de ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n✦ Ahora somos ${groupSize} Miembros.
-Te esperamos pronto!`
-    await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak)
-  }}
+  return true
+}
