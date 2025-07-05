00b5f21f9f7756d6eb31ebe0752d9363040bb298
Dev-Rayo <159085113+Eliasivan@users.noreply.github.com>
2025-07-05 16:41:44 -0300

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,49 +1,29 @@
-import { execSync } from 'child_process'
+import { exec } from 'child_process';
 
-var handler = async (m, { conn, text }) => {
-  await m.react('❤️')
-  await conn.reply(m.chat, 'Aguarde unos segundos...', m)
+let handler = async (m, { conn }) => {
+  m.reply(`${emoji2} Actualizando el bot...`);
 
-  try {
-    const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-    let messager = stdout.toString()
+  exec('git pull', (err, stdout, stderr) => {
+    if (err) {
+      conn.reply(m.chat, `${msm} Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
+      return;
+    }
 
-    if (/ya (estoy|está)[\s\w]*actualizad[ao]/i.test(messager)) {
-      await conn.reply(m.chat, 'Ya estoy re actualizado 💫', m)
-    } else if (/actualizad[ao]|changed|files? updated?/i.test(messager)) {
-      await conn.reply(m.chat, '¡Actualización completada! ✅', m)
+    if (stderr) {
+      console.warn('Advertencia durante la actualización:', stderr);
+    }
+
+    if (stdout.includes('Already up to date.')) {
+      conn.reply(m.chat, `${emoji4} El bot ya está actualizado.`, m);
     } else {
-      await conn.reply(m.chat, messager, m)
+      conn.reply(m.chat, `${emoji} Actualización realizada con éxito.\n\n${stdout}`, m);
     }
-  } catch {
-    try {
-      const status = execSync('git status --porcelain')
-      if (status.length > 0) {
-        const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
-          if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('BlackSession/') || line.includes('npm-debug.log')) {
-            return null
-          }
-          return '*→ ' + line.slice(3) + '*'
-        }).filter(Boolean)
-        if (conflictedFiles.length > 0) {
-          const errorMessage = `💭 Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.\nArchivos en conflicto:\n${conflictedFiles.join('\n')}`
-          await conn.reply(m.chat, errorMessage, m)
-        }
-      }
-    } catch (error) {
-      console.error(error)
-      let errorMessage2 = '⚠️ Ocurrió un error inesperado.'
-      if (error.message) {
-        errorMessage2 += '\n⚠️ Mensaje de error: ' + error.message
-      }
-      await conn.reply(m.chat, errorMessage2, m)
-    }
-  }
-}
+  });
+};
 
-handler.help = ['update', 'actualizar']
-handler.tags = ['owner']
-handler.command = ['update', 'actualizar']
-handler.rowner = true
+handler.help = ['update'];
+handler.tags = ['owner'];
+handler.command = ['update'];
+handler.rowner = true;
 
-export default handler
+export default handler;
