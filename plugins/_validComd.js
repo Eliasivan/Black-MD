eb1152316d46b7ed3c25263e6fe27fbbc88599cd
Dev-Rayo <159085113+Eliasivan@users.noreply.github.com>
2025-07-09 20:21:33 -0300

    Actualizar _validComd.js

--- plugins/_validComd.js
+++ plugins/_validComd.js
@@ -1,3 +1,24 @@
+function levenshtein(a, b) {
+  if (!a.length) return b.length;
+  if (!b.length) return a.length;
+  const matrix = Array.from({ length: b.length + 1 }, (_, i) => [i]);
+  for (let j = 0; j <= a.length; j++) matrix[0][j] = j;
+  for (let i = 1; i <= b.length; i++) {
+    for (let j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) === a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1,
+          matrix[i][j - 1] + 1,
+          matrix[i - 1][j] + 1
+        );
+      }
+    }
+  }
+  return matrix[b.length][a.length];
+}
+
 export async function before(message) {
   if (!message.text || !global.prefix.test(message.text)) return;
 
@@ -21,9 +42,29 @@ export async function before(message) {
     if (!userData.commands) userData.commands = 0;
     userData.commands += 1;
   } else {
-    let som = 0;
+    let allCommands = [];
+    for (let plugin of Object.values(global.plugins)) {
+      if (plugin.command) {
+        if (Array.isArray(plugin.command)) {
+          allCommands.push(...plugin.command.map(cmd => String(cmd)));
+        } else {
+          allCommands.push(String(plugin.command));
+        }
+      }
+    }
+    let minDist = Infinity, bestMatch = "", bestSimilarity = 0;
+    for (let cmd of allCommands) {
+      let dist = levenshtein(commandName, cmd.toLowerCase());
+      let similarity = Math.max(0, 100 - Math.round((dist / Math.max(commandName.length, cmd.length)) * 100));
+      if (dist < minDist || (dist === minDist && similarity > bestSimilarity)) {
+        minDist = dist;
+        bestMatch = cmd;
+        bestSimilarity = similarity;
+      }
+    }
+
     await message.reply(
-      `⏤͟͟͞͞El comando *${prefixUsed}${commandName}* no existe.\nPara ver la lista de comandos usa:\n» *${prefixUsed}Menu*\n❕ *Similitud:* _${parseInt(som)}%_`
+      `⏤͟͟͞͞El comando *${prefixUsed}${commandName}* no existe.\nQuizás quisiste decir: *${prefixUsed}${bestMatch}*\nPara ver la lista de comandos usa:\n» *${prefixUsed}Menu*\n❕ *Similitud:* _${bestSimilarity}%_`
     );
   }
 }
