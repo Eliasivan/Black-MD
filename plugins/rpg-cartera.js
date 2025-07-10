82a1dc30504a7cdaddcc5aa709d3bc664f67663e
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-07-10 14:39:32 -0300

    Actualizar rpg-cartera.js

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -1,12 +1,14 @@
 let handler = async (m, {conn, usedPrefix}) => {
 let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
 if (who == conn.user.jid) return error 
-if (!(who in global.db.data.users)) return conn.reply(m.chat, '🍭 El usuario no se encuentra en mi base de Datos.', m, fake)
+if (!(who in global.db.data.users)) return conn.reply(m.chat, `${emoji4} El usuario no se encuentra en mi base de Datos.`, m)
 let user = global.db.data.users[who]
-await m.reply(`${who == m.sender ? `Tienes *${user.money} Chocolates 🍫* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.money} Chocolates 🍫* en su Cartera`}. `, null, { mentions: [who] })}
+await m.reply(`${who == m.sender ? `Tienes *${user.coin} ${moneda} 💸* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.money} ${moneda} 💸* en su Cartera`}. `, null, { mentions: [who] })}
 
-handler.help = ['chocolates']
-handler.tags = ['rpg']
-handler.command = ['wallet', 'cartera', 'chocolates', 'bal', 'coins']
-handler.register = true 
+handler.help = ['wallet']
+handler.tags = ['economy']
+handler.command = ['wallet', 'cartera']
+handler.group = true
+handler.register = true
+
 export default handler
