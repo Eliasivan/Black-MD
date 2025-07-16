79f65ccf282153d650d63123cbf13aa231162f8c
Dev-Rayo <159085113+Eliasivan@users.noreply.github.com>
2025-07-16 12:53:38 -0300

    Goku-Black-Bot-MD

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -22,7 +22,7 @@ let handler = async (m, { args, conn }) => {
     }
 }
 
-handler.yenes = 2
+handler.coin = 2
 handler.command = ['ig', 'igdl', 'instagram'];
 handler.tags = ['dl'];
 handler.help = ['ig *<link>*'];
