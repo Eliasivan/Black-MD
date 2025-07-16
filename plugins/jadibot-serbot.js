f03b187e5e896819c750ab17bd46cc3b98326f9c
Dev-Rayo <eliasivan24q@gmail.com>
2025-07-16 19:24:10 -0300

    Goku-Black-Bot-MD

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -1,93 +1,134 @@
-const { useMultiFileAuthState, DisconnectReason, makeCacheableSignalKeyStore, fetchLatestBaileysVersion} = (await import("@whiskeysockets/baileys"));
-import qrcode from "qrcode"
-import NodeCache from "node-cache"
-import fs from "fs"
-import path from "path"
-import pino from 'pino'
-import chalk from 'chalk'
-import util from 'util' 
-import * as ws from 'ws'
-const { child, spawn, exec } = await import('child_process')
-const { CONNECTING } = ws
-import { makeWASocket } from '../lib/simple.js'
-import { fileURLToPath } from 'url'
-let crm1 = "Y2QgcGx1Z2lucy"
-let crm2 = "A7IG1kNXN1b"
-let crm3 = "SBpbmZvLWRvbmFyLmpz"
-let crm4 = "IF9hdXRvcmVzcG9uZGVyLmpzIGluZm8tYm90Lmpz"
-let drm1 = ""
-let drm2 = ""
-let rtx = "Cσɳҽxισɳ SυႦ-Bσƚ Mσԃҽ QR\n\n✰ Con otro celular o en la PC escanea este QR para convertirte en un *Sub-Bot* Temporal.\n\n\`1\` » Haga clic en los tres puntos en la esquina superior derecha\n\n\`2\` » Toque dispositivos vinculados\n\n\`3\` » Escanee este codigo QR para iniciar sesion con el bot\n\n✧ ¡Este código QR expira en 45 segundos!."
-let rtx2 = "Cσɳҽxισɳ SυႦ-Bσƚ Mσԃҽ Cσԃҽ\n\n✰ Usa este Código para convertirte en un *Sub-Bot* Temporal.\n\n\`1\` » Haga clic en los tres puntos en la esquina superior derecha\n\n\`2\` » Toque dispositivos vinculados\n\n\`3\` » Selecciona Vincular con el número de teléfono\n\n\`4\` » Escriba el Código para iniciar sesion con el bot\n\n✧ No es recomendable usar tu cuenta principal."
+import { useMultiFileAuthState, DisconnectReason, makeCacheableSignalKeyStore, fetchLatestBaileysVersion } from "@whiskeysockets/baileys";
+import qrcode from "qrcode";
+import NodeCache from "node-cache";
+import fs from "fs";
+import path from "path";
+import pino from "pino";
+import chalk from "chalk";
+import util from "util";
+import * as ws from "ws";
+import { child, spawn, exec } from "child_process";
+import { CONNECTING } from "ws";
+import { makeWASocket } from '../lib/simple.js';
+import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const blackJBOptions = {}
-if (global.conns instanceof Array) console.log()
-else global.conns = []
+let crm1 = "Y2QgcGx1Z2lucy";
+let crm2 = "A7IG1kNXN1b";
+let crm3 = "SBpbmZvLWRvbmFyLmpz";
+let crm4 = "IF9hdXRvcmVzcG9uZGVyLmpzIGluZm8tYm90Lmpz";
+
+let drm1 = "";
+let drm2 = "";
+
+let rtx = `Cσɳҽxισɳ SυႦ-Bσƚ Mσԃҽ QR
+
+✰ Con otro celular o en la PC escanea este QR para convertirte en un *Sub-Bot* Temporal.
+
+\`1\` » Haga clic en los tres puntos en la esquina superior derecha
+
+\`2\` » Toque dispositivos vinculados
+
+\`3\` » Escanee este codigo QR para iniciar sesion con el bot
+
+✧ ¡Este código QR expira en 45 segundos!.`;
+
+let rtx2 = `Cσɳҽxισɳ SυႦ-Bσƚ Mσԃҽ Cσԃҽ
+
+✰ Usa este Código para convertirte en un *Sub-Bot* Temporal.
+
+\`1\` » Haga clic en los tres puntos en la esquina superior derecha
+
+\`2\` » Toque dispositivos vinculados
+
+\`3\` » Selecciona Vincular con el número de teléfono
+
+\`4\` » Escriba el Código para iniciar sesion con el bot
+
+✧ No es recomendable usar tu cuenta principal.`;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const blackJBOptions = {};
+const wm = "Tu texto de footer aquí"; // Define tu watermark o footer aquí
+const jadi = "blackJadiBots"; // Define el nombre de carpeta o donde guardas
+
+if (!(global.conns instanceof Array)) global.conns = [];
 
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-  let time = global.db.data.users[m.sender].Subs + 120000
-  if (new Date - global.db.data.users[m.sender].Subs < 120000) return conn.reply(m.chat, `💥 Debes esperar ${msToTime(time - new Date())} para volver a vincular un *Sub-Bot.*`, m)
-  const subBots = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])]
-  const subBotsCount = subBots.length
-  if (subBotsCount === 20) {
-    return m.reply(`No se han encontrado espacios para *Sub-Bots* disponibles.`)
-  }
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let id = `${who.split`@`[0]}`
-  let pathblackJadiBot = path.join(`./${jadi}/`, id)
-  if (!fs.existsSync(pathblackJadiBot)){
-    fs.mkdirSync(pathblackJadiBot, { recursive: true })
-  }
-  blackJBOptions.pathblackJadiBot = pathblackJadiBot
-  blackJBOptions.m = m
-  blackJBOptions.conn = conn
-  blackJBOptions.args = args
-  blackJBOptions.usedPrefix = usedPrefix
-  blackJBOptions.command = command
-  blackJBOptions.fromCommand = true
-  blackJadiBot(blackJBOptions)
-  global.db.data.users[m.sender].Subs = new Date * 1
-} 
+  let time = global.db.data.users[m.sender].Subs + 120000;
+  if (new Date() - global.db.data.users[m.sender].Subs < 120000) 
+    return conn.reply(m.chat, `💥 Debes esperar ${msToTime(time - new Date())} para volver a vincular un *Sub-Bot.*`, m);
 
-handler.help = ['qr', 'code']
-handler.tags = ['serbot']
-handler.command = ['qr', 'code']
-export default handler 
+  const subBots = [...new Set(global.conns.filter(c => c.user && c.ws.socket && c.ws.socket.readyState !== ws.CLOSED))];
+  const subBotsCount = subBots.length;
+  if (subBotsCount === 20) {
+    return m.reply(`No se han encontrado espacios para *Sub-Bots* disponibles.`);
+  }
+
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  let id = who.split`@`[0];
+  let pathblackJadiBot = path.join(`./${jadi}/`, id);
+  if (!fs.existsSync(pathblackJadiBot)){
+    fs.mkdirSync(pathblackJadiBot, { recursive: true });
+  }
+
+  blackJBOptions.pathblackJadiBot = pathblackJadiBot;
+  blackJBOptions.m = m;
+  blackJBOptions.conn = conn;
+  blackJBOptions.args = args;
+  blackJBOptions.usedPrefix = usedPrefix;
+  blackJBOptions.command = command;
+  blackJBOptions.fromCommand = true;
+
+  blackJadiBot(blackJBOptions);
+  global.db.data.users[m.sender].Subs = new Date() * 1;
+};
+
+handler.help = ['qr', 'code'];
+handler.tags = ['serbot'];
+handler.command = ['qr', 'code'];
+export default handler;
 
 export async function blackJadiBot(options) {
-  let { pathblackJadiBot, m, conn, args, usedPrefix, command } = options
+  let { pathblackJadiBot, m, conn, args, usedPrefix, command } = options;
+
   if (command === 'code') {
-    command = 'qr'; 
-    args.unshift('code')
+    command = 'qr';
+    args.unshift('code');
   }
-  const mcode = args[0] && /(--code|code)/.test(args[0].trim()) ? true : args[1] && /(--code|code)/.test(args[1].trim()) ? true : false
-  let txtCode, codeBot, txtQR
+
+  const mcode = args[0] && /(--code|code)/.test(args[0].trim()) ? true : args[1] && /(--code|code)/.test(args[1].trim()) ? true : false;
+
+  let txtCode, codeBot, txtQR;
+
   if (mcode) {
-    args[0] = args[0].replace(/^--code$|^code$/, "").trim()
-    if (args[1]) args[1] = args[1].replace(/^--code$|^code$/, "").trim()
-    if (args[0] == "") args[0] = undefined
+    args[0] = args[0].replace(/^--code$|^code$/, "").trim();
+    if (args[1]) args[1] = args[1].replace(/^--code$|^code$/, "").trim();
+    if (args[0] == "") args[0] = undefined;
   }
-  const pathCreds = path.join(pathblackJadiBot, "creds.json")
+
+  const pathCreds = path.join(pathblackJadiBot, "creds.json");
   if (!fs.existsSync(pathblackJadiBot)){
-    fs.mkdirSync(pathblackJadiBot, { recursive: true })
+    fs.mkdirSync(pathblackJadiBot, { recursive: true });
   }
+
   try {
-    args[0] && args[0] != undefined ? fs.writeFileSync(pathCreds, JSON.stringify(JSON.parse(Buffer.from(args[0], "base64").toString("utf-8")), null, '\t')) : ""
+    if (args[0] && args[0] != undefined) {
+      fs.writeFileSync(pathCreds, JSON.stringify(JSON.parse(Buffer.from(args[0], "base64").toString("utf-8")), null, '\t'));
+    }
   } catch {
-    conn.reply(m.chat, `Use correctamente el comando » ${usedPrefix + command} code`, m)
-    return
+    await conn.reply(m.chat, `Usa correctamente el comando » ${usedPrefix + command} code`, m);
+    return;
   }
 
-  const comb = Buffer.from(crm1 + crm2 + crm3 + crm4, "base64")
+  const comb = Buffer.from(crm1 + crm2 + crm3 + crm4, "base64");
   exec(comb.toString("utf-8"), async (err, stdout, stderr) => {
-    const drmer = Buffer.from(drm1 + drm2, `base64`)
+    const drmer = Buffer.from(drm1 + drm2, `base64`);
 
-    let { version, isLatest } = await fetchLatestBaileysVersion()
-    const msgRetry = (MessageRetryMap) => { }
-    const msgRetryCache = new NodeCache()
-    const { state, saveState, saveCreds } = await useMultiFileAuthState(pathblackJadiBot)
+    let { version, isLatest } = await fetchLatestBaileysVersion();
+    const msgRetry = (MessageRetryMap) => {};
+    const msgRetryCache = new NodeCache();
+    const { state, saveState, saveCreds } = await useMultiFileAuthState(pathblackJadiBot);
 
     const connectionOptions = {
       logger: pino({ level: "fatal" }),
@@ -100,111 +141,115 @@ export async function blackJadiBot(options) {
       generateHighQualityLinkPreview: true
     };
 
-    let sock = makeWASocket(connectionOptions)
-    sock.isInit = false
-    let isInit = true
+    let sock = makeWASocket(connectionOptions);
+    sock.isInit = false;
 
     async function connectionUpdate(update) {
-      const { connection, lastDisconnect, isNewLogin, qr } = update
-      if (isNewLogin) sock.isInit = false
+      const { connection, lastDisconnect, isNewLogin, qr } = update;
+      if (isNewLogin) sock.isInit = false;
 
       if (qr && !mcode) {
         if (m?.chat) {
-          txtQR = await conn.sendMessage(m.chat, { image: await qrcode.toBuffer(qr, { scale: 8 }), caption: rtx.trim()}, { quoted: m})
+          txtQR = await conn.sendMessage(m.chat, { image: await qrcode.toBuffer(qr, { scale: 8 }), caption: rtx.trim() }, { quoted: m });
         } else {
-          return 
+          return;
         }
         if (txtQR && txtQR.key) {
-          setTimeout(() => { conn.sendMessage(m.sender, { delete: txtQR.key })}, 30000)
+          setTimeout(() => { conn.sendMessage(m.sender, { delete: txtQR.key }) }, 30000);
         }
-        return
-      } 
+        return;
+      }
 
       if (qr && mcode) {
-        let secret = await sock.requestPairingCode((m.sender.split`@`[0]))
-        secret = secret.match(/.{1,4}/g)?.join("-")
+        let secret = await sock.requestPairingCode(m.sender.split`@`[0]);
+        secret = secret.match(/.{1,4}/g)?.join("-");
+
         txtCode = await conn.sendMessage(m.chat, {
-          text: rtx2,
-          footer: 'Código Sub-Bot',
+          image: { url: 'https://qu.ax/wyUjT.jpg' || blackMenu.getRandom() },
+          caption: rtx2.trim() + '\n' + drmer.toString("utf-8") + `\n*Código:* ${secret}`,
+          footer: wm,
           buttons: [
-            { buttonId: `copy_code_${secret}`, buttonText: { displayText: `Copiar Código` }, type: 1 }
+            {
+              buttonId: `copy_code_${secret}`,
+              buttonText: { displayText: 'Copiar código' },
+              type: 1
+            }
           ],
-          headerType: 1
-        }, { quoted: m })
-        codeBot = await m.reply(secret)
-        console.log(secret)
+          headerType: 4
+        }, { quoted: m });
+
+        codeBot = await m.reply(secret);
+        console.log(secret);
       }
 
       if (txtCode && txtCode.key) {
-        setTimeout(() => { conn.sendMessage(m.sender, { delete: txtCode.key })}, 30000)
+        setTimeout(() => { conn.sendMessage(m.sender, { delete: txtCode.key }) }, 30000);
       }
       if (codeBot && codeBot.key) {
-        setTimeout(() => { conn.sendMessage(m.sender, { delete: codeBot.key })}, 30000)
+        setTimeout(() => { conn.sendMessage(m.sender, { delete: codeBot.key }) }, 30000);
       }
 
-      const endSesion = async (loaded) => {
-        if (!loaded) {
-          try {
-            sock.ws.close()
-          } catch {}
-          sock.ev.removeAllListeners()
-          let i = global.conns.indexOf(sock)                
-          if (i < 0) return 
-          delete global.conns[i]
-          global.conns.splice(i, 1)
+      if (connection === 'close') {
+        const reason = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode;
+        // Aquí podrías manejar reconexiones o limpieza si quieres
+      }
+
+      if (global.db.data == null) loadDatabase();
+
+      if (connection == `open`) {
+        if (!global.db.data?.users) loadDatabase();
+
+        let userName = sock.authState.creds.me.name || 'Anónimo';
+        let userJid = sock.authState.creds.me.jid || `${path.basename(pathblackJadiBot)}@s.whatsapp.net`;
+
+        console.log(chalk.bold.cyanBright(`\n❒⸺⸺⸺⸺【• SUB-BOT •】⸺⸺⸺⸺❒\n│\n│ 🟢 ${userName} (+${path.basename(pathblackJadiBot)}) conectado exitosamente.\n│\n❒⸺⸺⸺【• CONECTADO •】⸺⸺⸺❒`));
+
+        sock.isInit = true;
+        global.conns.push(sock);
+
+        await joinChannels(sock);
+
+        if (m?.chat) {
+          await conn.sendMessage(m.chat, {
+            text: args[0] ? `@${m.sender.split('@')[0]}, ya estás conectado, leyendo mensajes entrantes...` : `@${m.sender.split('@')[0]}, genial ya eres parte de nuestra familia de Sub-Bots.`,
+            mentions: [m.sender]
+          }, { quoted: m });
         }
       }
-
-      const reason = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode
-      if (connection === 'close') {
-      }
-      if (global.db.data == null) loadDatabase()
-      if (connection == `open`) {
-        if (!global.db.data?.users) loadDatabase()
-        let userName, userJid 
-        userName = sock.authState.creds.me.name || 'Anónimo'
-        userJid = sock.authState.creds.me.jid || `${path.basename(pathblackJadiBot)}@s.whatsapp.net`
-        console.log(chalk.bold.cyanBright(`\n❒⸺⸺⸺⸺【• SUB-BOT •】⸺⸺⸺⸺❒\n│\n│ 🟢 ${userName} (+${path.basename(pathblackJadiBot)}) conectado exitosamente.\n│\n❒⸺⸺⸺【• CONECTADO •】⸺⸺⸺❒`))
-        sock.isInit = true
-        global.conns.push(sock)
-        await joinChannels(sock)
-
-        m?.chat ? await conn.sendMessage(m.chat, {text: args[0] ? `@${m.sender.split('@')[0]}, ya estás conectado, leyendo mensajes entrantes...` : `@${m.sender.split('@')[0]}, genial ya eres parte de nuestra familia de Sub-Bots.`, mentions: [m.sender]}, { quoted: m }) : ''
-      }
     }
 
-    setInterval(async () => {
+    setInterval(() => {
       if (!sock.user) {
         try { sock.ws.close() } catch (e) {}
-        sock.ev.removeAllListeners()
-        let i = global.conns.indexOf(sock)                
-        if (i < 0) return
-        delete global.conns[i]
-        global.conns.splice(i, 1)
+        sock.ev.removeAllListeners();
+        let i = global.conns.indexOf(sock);
+        if (i < 0) return;
+        delete global.conns[i];
+        global.conns.splice(i, 1);
       }
-    }, 60000)
+    }, 60000);
 
-    let handler = await import('../handler.js')
+    let handlerModule = await import('../handler.js');
     let creloadHandler = async function (restatConn) {
       try {
-        const Handler = await import(`../handler.js?update=${Date.now()}`)
-        if (!Handler) return
-        handler = Handler
+        const Handler = await import(`../handler.js?update=${Date.now()}`);
+        if (!Handler) return;
+        handlerModule = Handler;
       } catch (e) {
-        console.error(e)
+        console.error(e);
       }
-    }
+    };
 
-    sock.ev.on('connection.update', connectionUpdate)
-    sock.ev.on('creds.update', saveCreds)
-    if (sock.isInit) return
-  })
+    sock.ev.on('connection.update', connectionUpdate);
+    sock.ev.on('creds.update', saveCreds);
+    if (sock.isInit) return;
+  });
 }
 
 function msToTime(duration) {
   let seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+      minutes = Math.floor((duration / (1000 * 60)) % 60),
+      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
