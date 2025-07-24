7bb1145586884688c2c2654ced8da70e82571fdb
Dev-Rayo <eliasivan24q@gmail.com>
2025-07-24 17:52:43 -0300

    update

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -122,7 +122,7 @@ const handler = async (m, { conn, usedPrefix: _p, command }) => {
       .replace(/%rtotalreg/g, rtotalreg)
       .replace(/%date/g, fecha)
 
-    const messageContent = {
+   /* const messageContent = {
       viewOnceMessage: {
         message: {
           messageContextInfo: {
@@ -153,7 +153,7 @@ const handler = async (m, { conn, usedPrefix: _p, command }) => {
           })
         }
       }
-    }
+    }/*
 
     const msg = generateWAMessageFromContent(m.chat, messageContent, {
       userJid: m.sender,
