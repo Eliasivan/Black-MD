f7b206d1f4c708d122a568d44cbcf71670a503b1
Dev-Rayo <159085113+Eliasivan@users.noreply.github.com>
2025-08-02 16:49:46 -0300

    xdd

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.owner = [
   ['5351524614'],
 //lid 
   ['70639914369141@lid', 'Sayan', true,
-  ['174560573964411@lid', 'Sayan', true,
+  ['174560573964411@lid', 'Rayo', true]
 ];
 
 global.mods = []
