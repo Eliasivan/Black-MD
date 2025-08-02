25b28781b1eb580a20d9d7e6eed46572d7a89d2c
Dev-Rayo <159085113+Eliasivan@users.noreply.github.com>
2025-08-02 19:45:00 -0300

    Actualizar dl-gitclone.js

--- plugins/dl-gitclone.js
+++ plugins/dl-gitclone.js
@@ -1,32 +1,48 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
-const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
+let regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
+let handler = async (m, { args, usedPrefix, command }) => {
+  if (!args[0]) {
+    return conn.reply(m.chat, `${emoji} Por favor, ingresa la URL de un repositorio de GitHub que deseas descargar.`, m)
+  }
+  if (!regex.test(args[0])) {
+    return conn.reply(m.chat, `${emoji2} Verifica que la *URL* sea de GitHub`, m).then(_ => m.react(error))
+  }
+  let [_, user, repo] = args[0].match(regex) || []
+  let sanitizedRepo = repo.replace(/.git$/, '')
+  let repoUrl = `https://api.github.com/repos/${user}/${sanitizedRepo}`
+  let zipUrl = `https://api.github.com/repos/${user}/${sanitizedRepo}/zipball`
+  await m.react(rwait)
+  try {
+  conn.reply(m.chat, wait, m)
+    let [repoResponse, zipResponse] = await Promise.all([
+      fetch(repoUrl),
+      fetch(zipUrl),
+    ])
+    let repoData = await repoResponse.json()
+    let filename = zipResponse.headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+    let type = zipResponse.headers.get('content-type')
+    let img = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745610598914.jpeg'
+    let txt = `*乂  G I T H U B  -  D O W N L O A D*\n\n`
+       txt += `✩  *Nombre* : ${sanitizedRepo}\n`
+       txt += `✩  *Repositorio* : ${user}/${sanitizedRepo}\n`
+       txt += `✩  *Creador* : ${repoData.owner.login}\n`
+       txt += `✩  *Descripción* : ${repoData.description || 'Sin descripción disponible'}\n`
+       txt += `✩  *Url* : ${args[0]}\n\n`
+       txt += `> ${dev}`
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    try {
-        if (!args.length) 
-            return conn.reply(m.chat, `Ingresa un link de GitHub`, m);
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
+await conn.sendFile(m.chat, await zipResponse.buffer(), filename, null, m)
+await m.react(done)
+  } catch {
+await m.react(error)
+  }
+}
+handler.help = ['gitclone *<url git>*']
+handler.tags = ['descargas']
+handler.command = ['gitclone']
+handler.group = true
+handler.register = true
+handler.coin = 3
 
-        if (!regex.test(args[0])) 
-            return conn.reply(m.chat, `⚠️ Ingresa un link válido de GitHub.`, m);
-
-        let [_, user, repo] = args[0].match(regex) || [];
-        repo = repo.replace(/.git$/, '');
-        let url = `https://api.github.com/repos/${user}/${repo}/zipball`;
-
-        let response = await fetch(url, { method: 'HEAD' });
-        let filename = response.headers.get('content-disposition').match(/attachment; filename=(.*)/)[1];
-
-        m.react('⌛');
-        await conn.sendFile(m.chat, url, filename, null, m);
-        m.react('🌱');
-    } catch (e) {
-        return conn.reply(m.chat, JSON.stringify({ error: e.message }, null, 2), m);
-    }
-};
-
-handler.help = ["gitclone"];
-handler.command = ["gitclone"];
-handler.tags = ["download"];
-
-export default handler;
+export default handler
